f2f51b9d980a065cef53a6bfe836ad5c4b959f0d
Luiz Parente <lp.parente@gmail.com>
2021-03-22 21:25:29 -0400

    Creating a WPF UI.

---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+
+			// Customizations go after this line!
+
+			// Accessing controls programatically.
+			txtFirstName.Text = "Hello";
+			txtLastName.Text = "World!";
+
+			// Installing event listeners
+			btnSave.Click += SaveButtonClick;
+		}
+
+		public void SaveButtonClick(object sender, EventArgs e) {
+			MessageBox.Show("Save button clicked!");
+		}
+	}
+}
